893b9de1368d70dfaca6b53578a91b1fe90a322b
feyzarafiq <feyza_rafiq@hotmail.de>
2023-08-30 21:49:35 +0200

    change rextangleAreaStrategy logic easier

--- shapeArea-task-1/strategies/RectangleAreaStrategy.java
+++ shapeArea-task-1/strategies/RectangleAreaStrategy.java
@@ -1,10 +1,11 @@
 package strategies;
+
 public class RectangleAreaStrategy implements AreaCalculationStrategy {
     @Override
     public double calculateArea(Shape shape) {
-        if (shape instanceof Rectangle) {
-            Rectangle rectangle = (Rectangle) shape;
-            return rectangle.getArea();
-        }
-        return 0.0;
-}}
+
+        Rectangle rectangle = (Rectangle) shape;
+        return rectangle.getArea();
+
+    }
+}
